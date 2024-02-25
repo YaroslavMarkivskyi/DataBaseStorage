63542c279a6a0842b6eb48cfc9f801a217e47a05
Yaroslav Markivskyi <129275754+YaroslavMarkivskyi@users.noreply.github.com>
2024-02-25 13:42:30 +0200

    Refactored config

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     """
     Configuration class to store all the configuration variables.
     """
-    DB_HOST = config['database']['host']
+    DB_HOST = config['database']['db_host']
     DB_NAME = config['database']['db_name']
     DB_USER = config['database']['db_user']
     DB_PASS = config['database']['db_pass']
-    DB_PORT = config['database']['port']
+    DB_PORT = config['database']['db_port']
