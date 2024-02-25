488c8f600fea7ba5158928b0a5a9ad082c022a43
Yaroslav Markivskyi <129275754+YaroslavMarkivskyi@users.noreply.github.com>
2024-02-25 13:35:14 +0200

    Refactored config

--- config.py
+++ config.py
@@ -11,6 +11,5 @@ class Config:
     DB_HOST = config['database']['host']
     DB_NAME = config['database']['db_name']
     DB_USER = config['database']['db_user']
-    DB_PASSWORD = config['database']['db_password']
+    DB_PASS = config['database']['db_pass']
     DB_PORT = config['database']['port']
-    DB_URL = config['database']['url']
